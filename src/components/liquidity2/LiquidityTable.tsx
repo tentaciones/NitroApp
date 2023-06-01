05df0f4904b42e3fb29e1adaf7f2c8db70d1fc77
tentaciones <chideracharles987@gmail.com>
2023-06-01 07:51:16 +0100

    added the ability to show card with more information onhover for liquidityTable

--- src/components/liquidity2/LiquidityTable.tsx
+++ src/components/liquidity2/LiquidityTable.tsx
@@ -70,6 +70,37 @@ const LiquidityTable = (props: Props) => {
                                 {assetXSymbol}/{assetYSymbol}
                               </p>
                             </div>
+                            <div className="absolute -mt-10 h-[70px] w-[200px] bg-[#091E33]  hover:opacity-100 opacity-0 transition duration-150 ease-in-out hover:cursor-pointer">
+                              <div className="flex px-5 items-center mt-2">
+                                <Image
+                                  src={assetXLogo.src}
+                                  alt=""
+                                  width="0"
+                                  height="0"
+                                  className="-mx-2 h-5 w-5 "
+                                />
+                                <Image
+                                  src={assetYLogo.src}
+                                  alt=""
+                                  width="0"
+                                  height="0"
+                                  className="  h-5 w-5"
+                                />
+                                <p className="px-2 text-sm font-thin">
+                                  {assetXSymbol}/{assetYSymbol}
+                                </p>
+                              </div>
+                              <div className="flex flex-col px-3 mt-1">
+                                <div className="flex justify-between text-[10px] font-thin ">
+                                  <p>ETH</p>
+                                  <p>0.555</p>
+                                </div>
+                                <div className="flex justify-between text-[10px] font-thin ">
+                                  <p>USDT</p>
+                                  <p>555</p>
+                                </div>
+                              </div>
+                            </div>
                           </td>
                           <td className="whitespace-nowrap px-2 py-4 w-[10%] ">
                             {TVL}
